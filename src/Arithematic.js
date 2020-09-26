ba8244b198e6f7ddd0f4d79cbe9b3ad8fd12917b
Gyandev Pandey <gyandev_pandey@persistent.co.in>
2020-09-26 12:16:32 +0530

    JSX Expression

---
+++ src/Arithematic.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+let add = <h1>Value:{10+20}</h1>;
+
+export default add;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import add from "./Arithematic";
 
-import App from "./App"
-ReactDOM.render(<App/>,document.getElementById("root"));
+ReactDOM.render(add,document.getElementById("root"));
+// import App from "./App"
+// ReactDOM.render(<App/>,document.getElementById("root"));
